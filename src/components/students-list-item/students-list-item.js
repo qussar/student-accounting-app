79a043725c79ce8ef97432dc6e06976865f65d7d
Maksim <qussardev@gmail.com>
2022-09-18 01:14:40 +0300

    added onClick

--- src/components/students-list-item/students-list-item.js
+++ src/components/students-list-item/students-list-item.js
@@ -1,31 +1,62 @@
+import { Component } from 'react';
+
 import './students-list-item.css';
 
 
-const StudentsListItem = ({ name, stipend, increase }) => {
-
-   let classNames = "list-group-item d-flex justify-content-between";
-   if (increase) {
-      classNames += " increase";
+class StudentsListItem extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         increase: false,
+         rise: false
+      }
    }
 
-   return (
-      <li className={classNames}>
-         <span className="list-group-item-label">{name}</span>
-         <input type="text" className="list-group-item-input" defaultValue={stipend + "$"} />
-         <div className='d-flex justify-content-center align-items-center'>
-            <button type="button"
-               className="btn-cookie btn-sm ">
-               <i className="fas fa-cookie"></i>
-            </button>
+   onIncreuse = () => {
+      this.setState(({ increase }) => ({
+         increase: !increase
+      }))
+   }
 
-            <button type="button"
-               className="btn-trash btn-sm ">
-               <i className="fas fa-trash"></i>
-            </button>
-            <i className="fas fa-star"></i>
-         </div>
-      </li>
-   )
+   onRise = () => {
+      this.setState(({ rise }) => ({
+         rise: !rise
+      }))
+   }
+
+   render() {
+      const { name, stipend } = this.props;
+      const { increase, rise } = this.state;
+
+      let classNames = "list-group-item d-flex justify-content-between";
+      if (increase) {
+         classNames += " increase";
+      }
+      if (rise) {
+         classNames += ' like';
+      }
+
+      return (
+         <li className={classNames}>
+            <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={stipend + "$"} />
+            <div className='d-flex justify-content-center align-items-center'>
+               <button type="button"
+                  className="btn-cookie btn-sm "
+                  onClick={this.onIncreuse}>
+                  <i className="fas fa-cookie"></i>
+               </button>
+
+               <button type="button"
+                  className="btn-trash btn-sm ">
+                  <i className="fas fa-trash"></i>
+               </button>
+               <i className="fas fa-star"></i>
+            </div>
+         </li>
+      )
+   }
+
 }
 
 export default StudentsListItem;
